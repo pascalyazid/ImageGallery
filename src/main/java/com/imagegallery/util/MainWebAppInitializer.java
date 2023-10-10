ffa77133b67b35fa49e45e989a050b5c0d4f80e7
pascalyazid <pascal.thuma@gmail.com>
2023-10-10 12:17:45 +0200

    fixed Main class to work with new Spring version

--- src/main/java/com/imagegallery/util/MainWebAppInitializer.java
+++ src/main/java/com/imagegallery/util/MainWebAppInitializer.java
@@ -4,10 +4,8 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.MultipartConfigElement;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
+import jakarta.servlet.MultipartConfigElement;
+import jakarta.servlet.ServletRegistration;
 
 public class MainWebAppInitializer implements WebApplicationInitializer {
 
@@ -15,16 +13,16 @@ public class MainWebAppInitializer implements WebApplicationInitializer {
     private int MAX_UPLOAD_SIZE = 5 * 1024 * 1024;
 
     @Override
-    public void onStartup(ServletContext sc) throws ServletException {
+    public void onStartup(jakarta.servlet.ServletContext sc) throws jakarta.servlet.ServletException {
 
         ServletRegistration.Dynamic appServlet = sc.addServlet("mvc", new DispatcherServlet(
-                new GenericWebApplicationContext()));
+            new GenericWebApplicationContext()));
 
-        appServlet.setLoadOnStartup(1);
+    appServlet.setLoadOnStartup(1);
+    MultipartConfigElement multipartConfigElement = new MultipartConfigElement(TMP_FOLDER,
+            MAX_UPLOAD_SIZE, MAX_UPLOAD_SIZE * 2, MAX_UPLOAD_SIZE / 2);
 
-        MultipartConfigElement multipartConfigElement = new MultipartConfigElement(TMP_FOLDER,
-                MAX_UPLOAD_SIZE, MAX_UPLOAD_SIZE * 2, MAX_UPLOAD_SIZE / 2);
+    appServlet.setMultipartConfig(multipartConfigElement);
 
-        appServlet.setMultipartConfig(multipartConfigElement);
     }
 }
